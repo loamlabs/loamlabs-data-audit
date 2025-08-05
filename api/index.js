2a10c992929ab2e6c10e2510b426783cd32b7419
loamlabs <info@loamlabsusa.com>
2025-08-05 10:55:52 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-// Import the necessary tools (libraries) - test
+// Import the necessary tools (libraries)
 const shopifyApi = require('@shopify/shopify-api');
 require('@shopify/shopify-api/adapters/node');
 const { Resend } = require('resend');
